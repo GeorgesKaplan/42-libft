966ab9f79338203f40cb23f507be20b5a54ceff0
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-02-20 12:21:12 +0100

    Norme

--- srcs/libft_split_keep.c
+++ srcs/libft_split_keep.c
@@ -6,7 +6,7 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 11:40:57 by dnantet           #+#    #+#             */
-/*   Updated: 2026/02/20 12:17:27 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/02/20 12:20:43 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,15 @@ static void	split_keep_continued(char *str, char sep, char **arr,
 		if (*indexes[0] > *indexes[2])
 		{
 			arr[*indexes[3]] = ft_substr(str, *indexes[2],
-				*indexes[0] - *indexes[2]);
+					*indexes[0] - *indexes[2]);
 			if (!arr[(*indexes[3])++])
 				return (free_all(arr, (*indexes[3]) - 1));
 		}
 		*indexes[1] = *indexes[0] + 1;
 		while (str[*indexes[1]] && str[*indexes[1]] != sep)
 			(*indexes[1])++;
-		arr[*indexes[3]] = ft_substr(str, *indexes[0], (*indexes[1]) - *indexes[0]);
+		arr[*indexes[3]] = ft_substr(str, *indexes[0],
+				(*indexes[1]) - *indexes[0]);
 		if (!arr[(*indexes[3])++])
 			return (free_all(arr, (*indexes[3]) - 1));
 		*indexes[0] = *indexes[1];
@@ -55,7 +56,6 @@ static void	split_keep_continued(char *str, char sep, char **arr,
 		(*indexes[0])++;
 }
 
-
 /**
 * @brief Splits a string into an array of strings at the given separator,
 * keeping the separator at the start of the word.
@@ -63,7 +63,7 @@ static void	split_keep_continued(char *str, char sep, char **arr,
 char	**ft_split_keep(char *str, char sep)
 {
 	char	**arr;
-	int		indexes[4]; // i, j, start, idx
+	int		indexes[4];
 
 	arr = init_split_keep_arr(str, sep);
 	if (!arr)
